36a9979949c955899d38cc80b5ca22e29c24d9cf
Joabe Santana Correia <joabesantana.c@gmail.com>
2020-01-15 20:53:15 -0300

    Commit inical do Projeto

---
+++ Conversores/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Joabe Santana Correia on 15/01/20.
+//  Copyright © 2020 Joabe Santana Correia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textFieldValue: UITextField!
+    
+    @IBOutlet weak var btUnit1: UIButton!
+    
+    @IBOutlet weak var brUnit2: UIButton!
+    
+    @IBOutlet weak var labelResult: UILabel!
+    
+    @IBOutlet weak var labelResultUnit: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: Any) {
+    }
+}
+
